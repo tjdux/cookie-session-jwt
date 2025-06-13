dceba03addd330f333c9d0e15ea1477ce6ba3dd4
tjdux <tjduxxx@gmail.com>
2025-06-13 14:24:27 +0900

    회원가입 로직

--- middleware/error-handling-middleware.js
+++ middleware/error-handling-middleware.js
@@ -5,7 +5,7 @@ module.exports = function (err, req, res, next) {
       return res.status(400).send({
         errorMessage: "잘못된 비밀번호"
       })
-    case 'existing email':
+    case 'ExistingEmail':
       return res.status(400).send({
         errorMessage: "가입된 이메일"
       })
@@ -18,6 +18,19 @@ module.exports = function (err, req, res, next) {
     case "accessTokenNotMatched":
       return res.status(401).send({
         errorMessage: "로그인을 해주세요"
+      })
+    case "InputValidation":
+    case "PasswordValidation":
+      return res.status(401).send({
+        errorMessage: "입력된 요청이 잘못되었습니다."
+      })
+    case "DataBaseError":
+      return res.status(500).send({
+        errorMessage: "데이터 베이스 오류"
+      })
+    default:
+      return res.status(500).send({
+        errorMessage: "서버 오류"
       })
   }
 }
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -2,7 +2,58 @@ const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
 const authenticateToken = require('../middleware/authenticate-middleware')
+const prisma = require('../utils/prisma/index')
+const { body } = require('express-validator');
 require('dotenv').config()
+
+/** 회원가입
+ *  1. 닉네임, 이메일, 비밀번호 입력 검증
+ *  1-1. 비밀번호 6글자 이상
+ *  1-2. 이메일 중복 확인
+ *  2. 데이터 베이스에 저장 
+ */
+
+// 회원가입
+router.post('/sign-up', body('email').notEmpty(),  async (req, res, next) => {
+  const { email, password, nickname } = req.body;
+
+  // 1. 닉네임, 이메일, 비밀번호 입력 검증
+  if (!email || !password || !nickname) {
+    return next(new Error("InputValidation"))
+  }
+
+  // 1-1. 비밀번호 6글자 이상
+  if (password.length < 6) {
+    return next(new Error("PasswordValidation"))
+  }
+
+
+  try {
+    // 1-2. 이메일 중복 확인
+    const user = await prisma.user.findFirst({
+      where: { email }
+    })
+
+    if(user){    
+      return next(new Error("ExistingEmail"))
+    }
+
+    // 2. 데이터 베이스에 저장
+    await prisma.user.create({
+      data: {
+        email,
+        password,
+        nickname
+      }
+    })
+
+    return res.status(201).json({
+      message: "회원가입 완료"
+  })
+  } catch (e) {
+    return next(new Error("DataBaseError"))
+  }
+})
 
 router.get('/set-session', (req, res) => {
   console.log(req.session.users)
@@ -18,11 +69,6 @@ router.get('/set-session', (req, res) => {
 
 // 토큰 생성
 router.get('/login', (req, res, next) => {
-  // const {username, password} = req.params;
-  // req.session.user = {
-  //   username: user.username,
-  //   loginAt: new Date().toString()
-  // }
   const user = {
     id: 1,
     username: "john",
@@ -30,7 +76,7 @@ router.get('/login', (req, res, next) => {
   }
 
   const token = jwt.sign(user, process.env.SECRET_KEY, {
-    expiresIn: '5s'
+    expiresIn: '5h'
   })
   console.log(token);
 
@@ -42,6 +88,7 @@ router.get('/login', (req, res, next) => {
 router.get('/user', authenticateToken, (req, res, next) => {
   console.log(req.user);
   // next(new Error("existing email"));
+  res.send(req.user);
 })
 
 router.get('/set-cookie', (req, res) => { 
@@ -55,5 +102,11 @@ router.get('/get-cookie', (req, res) => {
     cookies
   })
 })
+
+const signUpValidator = [
+  body('email')
+    .isEmail().withMessage("이메일 형식이 아닙니다.")
+    .notEmpty().withMessage("이메일이 없습니다.")
+]
 
 module.exports = router;
